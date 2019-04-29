a8fa9aac35bdf8fd9d1c7c6d8f031970127f7f29
Daniil Maksymenko <konor00792@gmail.com>
2019-04-29 18:43:48 +0300

    Add files via upload

---
+++ API/run.bat
@@ -0,0 +1,6 @@
+cd Analyser-API
+start cmd /k python app.py
+cd ../ML-API
+start cmd /k python app.py
+cd ../Scraper-API
+start cmd /k python main.py
---
+++ API/run.sh
@@ -0,0 +1,6 @@
+cd Analyser-API
+terminal -e python app.py
+cd ../ML-API
+terminal -e python app.py
+cd ../Scraper-API
+terminal -e python main.py
